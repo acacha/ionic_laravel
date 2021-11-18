33df88a32c8cd0b2fa90c7991f7ae6533ec4e52f
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-11-18 11:30:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_laravel',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
